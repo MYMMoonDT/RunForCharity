1129c795e2204127416ac8c6e0e2d1fa450c9d9f
jingpeng <jingpeng@10.20.94.74>
2013-12-28 00:39:46 +0800

    slide menu added

--- src/edu/tongji/runforcharity/LoginActivity.java
+++ src/edu/tongji/runforcharity/LoginActivity.java
@@ -2,19 +2,33 @@ package edu.tongji.runforcharity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_login);
+		Button loginButton = (Button) findViewById(R.id.loginButton);
+		loginButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this,
+						MainBoardActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
---
+++ src/edu/tongji/runforcharity/MainBoardActivity.java
@@ -0,0 +1,42 @@
+package edu.tongji.runforcharity;
+
+import edu.tongji.runforcharity.layout.BidirSlidingLayout;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class MainBoardActivity extends Activity implements OnItemClickListener{
+	
+	private BidirSlidingLayout bidirSldingLayout;
+	
+	private ListView contentList;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mainboard);
+		
+		contentList = (ListView) findViewById(R.id.contentList);
+		contentList.setOnItemClickListener(this);
+		bidirSldingLayout = (BidirSlidingLayout) findViewById(R.id.bidir_sliding_layout);
+		bidirSldingLayout.setScrollEvent(contentList);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		switch (arg0.getId())
+        {
+        case R.id.rightcontentList:
+            bidirSldingLayout.showOrHideRightMenu();
+            break;
+        case R.id.leftcontentList:
+            bidirSldingLayout.showOrHideLeftMenu();
+            break;
+        case R.id.contentList:
+            break;
+        }
+	}
+}
